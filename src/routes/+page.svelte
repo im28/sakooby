ab9ccea36ebd4c38041d374bc8fcabb2395b35e2
im28 <ibrahim.elshamandi@gmail.com>
2023-12-28 23:24:11 +0800

    Load door with states

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
-<script>
+<script lang="ts">
+	import '../app.css';
 	import Door from './Door.svelte';
 	import Sakooby from './Sakooby.svelte';
+	import type { Rive } from '@rive-app/canvas';
+	let riveInstance: Rive;
+	
+
 </script>
 
 <svelte:head>
@@ -8,7 +13,7 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class=" bg-black flex items-center flex-col justify-center p-[15%]">
+<div class=" fixed inset-0 bg-black flex items-center flex-col justify-center p-[15%]">
 	<Sakooby className="stroke-pink-300" />
-	<Door />
+	<Door bind:riveInstance />
 </div>
--- src/routes/Door.svelte
+++ src/routes/Door.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-
+	import { gsap } from 'gsap';
+	import { type Rive } from '@rive-app/canvas';
+	export let riveInstance: Rive;
 	onMount(async () => {
-		const { Rive, Layout, Fit, Alignment } = await import('@rive-app/canvas');
+		const { Rive, Layout, Fit, Alignment, EventType } = await import('@rive-app/canvas');
 		const canvasElement = document.getElementById('rive_canvas') as HTMLCanvasElement;
-
-		let r = new Rive({
+		riveInstance = new Rive({
 			onLoad: () => {
-				r.resizeDrawingSurfaceToCanvas();
+				riveInstance.resizeDrawingSurfaceToCanvas();
 			},
 			src: '/door_opening_and_closing.riv',
 			canvas: canvasElement,
@@ -19,9 +20,46 @@
 				alignment: Alignment.Center
 			})
 		});
+		gsap.fromTo(
+			canvasElement,
+			{ opacity: 0 },
+			{
+				opacity: 1,
+				duration: 2
+				// delay: 1.5
+			}
+		);
+
+		const timeoutId = setTimeout(() => {
+			const smInputs = riveInstance.stateMachineInputs('State Machine 1');
+			const toggleInput = smInputs[0];
+			toggleInput.value = !toggleInput.value;
+		}, 500);
+
+		riveInstance.on(EventType.StateChange, (e) => {
+			console.log(e);
+
+			if (e.data == 'Fully Open') {
+				const canvas_parent = document.getElementById('canvas_parent');
+				gsap.to(canvas_parent, {
+					scale: 20,
+					duration: 8,
+					delay: 3
+				});
+			}
+		});
+		// return () => {
+		// 	clearTimeout(timeoutId);
+		// 	riveInstance.cleanup();
+		// };
 	});
+	function handleClick() {
+		const smInputs = riveInstance.stateMachineInputs('State Machine 1');
+		const toggleInput = smInputs[1];
+		toggleInput.value = !toggleInput.value;
+	}
 </script>
 
-<div>
+<div id="canvas_parent" on:click={handleClick} on:keydown role="button" tabindex="0">
 	<canvas id="rive_canvas" class=" min-w-full min-h-full"></canvas>
 </div>
