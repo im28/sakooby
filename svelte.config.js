b27a647b3dc1616192fb78aa3afaebe4648c15d8
im28 <ibrahim.elshamandi@gmail.com>
2023-12-29 15:12:03 +0800

    testing

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
 const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
